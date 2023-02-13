0ca5f1df6b586e2556db3a5884b3a4f31962f5af
soyun <soyoon4122@naver.com>
2023-02-13 17:23:07 +0900

    Design: Add global style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      home
+      <input type="text" />
+      <button>안녕</button>
+    </div>
+  );
 };
 
 export default Home;
