57583dd8e3f00390751b5fd288fd76cb9220eb25
hoheesu <hoheesu@naver.com>
2023-02-13 19:21:01 +0900

    Design: 카테고리 메뉴바 ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Reset } from 'styled-reset';
 import CategoryBar from './components/CategoryBar';
+import { useLocation } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0);
+  const location = useLocation();
 
   return (
     <>
       <Reset />
       <Outlet />
-      <CategoryBar/>
+      { location.pathname.includes('auth') ? null : <CategoryBar/>}
     </>
   );
 }
--- src/components/CategoryBar.tsx
+++ src/components/CategoryBar.tsx
@@ -8,29 +8,61 @@ import {
   HiOutlineClipboardList,
 } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 
 function CategoryBar() {
   const location = useLocation();
-  console.log(location)
+  console.log(location);
   return (
-    <ul>
+    <CategoryWrap>
       <li>
-        <Link to="/">
-          { location.pathname === "/" ? <HiHome /> : <HiOutlineHome />}
-        </Link>
+        <LinkStyle to="/">
+          {location.pathname === '/' ? (
+            <HiHome color="red" />
+          ) : (
+            <HiOutlineHome />
+          )}
+          <p>홈</p>
+        </LinkStyle>
       </li>
       <li>
-        <Link to="/recommend">
-        { location.pathname === "/recommend" ? <HiHeart /> : <HiOutlineHeart />}
-        </Link>
+        <LinkStyle to="/recommend">
+          {location.pathname === '/recommend' ? (
+            <HiHeart />
+          ) : (
+            <HiOutlineHeart />
+          )}
+          <p>맞춤 추천</p>
+        </LinkStyle>
       </li>
       <li>
-        <Link to="/allproducts">
-        { location.pathname === "/allproducts" ? <HiClipboardList /> : <HiOutlineClipboardList />}
-        </Link>
+        <LinkStyle to="/allproducts">
+          {location.pathname === '/allproducts' ? (
+            <HiClipboardList />
+          ) : (
+            <HiOutlineClipboardList />
+          )}
+        <p>전체 상품</p>
+        </LinkStyle>
       </li>
-    </ul>
+    </CategoryWrap>
   );
 }
-
+const CategoryWrap = styled.ul`
+  position: fixed;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  bottom: 0;
+  svg {
+    font-size: 24px;
+    color: black;
+  }
+`;
+  const LinkStyle = styled(Link)`
+    display: flex;
+    flex-direction: column;
+    align-items : center;
+    width: 100px;
+  `
 export default CategoryBar;
