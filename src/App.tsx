3d77a956af68a536b3967e376ae1b2a0dab2b896
hoheesu <hoheesu@naver.com>
2023-02-13 21:22:28 +0900

    fix: Feat: reset style 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Reset } from 'styled-reset';
 import CategoryBar from './components/common/CategoryBar';
 import GlobalStyle from './style/globalStyles';
 import { useLocation } from 'react-router-dom'
@@ -11,7 +10,6 @@ function App() {
 
   return (
     <>
-      <Reset />
       <Outlet />
       <GlobalStyle />
       { location.pathname.includes('auth') ? null : <CategoryBar/>}
