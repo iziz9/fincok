38ed1765385d8c42e07e2edf13590b6d6268d3c2
hoheesu <hoheesu@naver.com>
2023-02-15 13:05:24 +0900

    feat: 마이페이지 컴포넌트 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import MyProfile from '../components/common/MyProfile';
+
 
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <div>
+      <MyProfile />
+    </div>
+  )
 };
 
 export default MyPage;
