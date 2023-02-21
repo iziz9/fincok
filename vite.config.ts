59182fc94058131f690a2c84fa102854ae072bd4
hoheesu <hoheesu@naver.com>
2023-02-21 15:35:39 +0900

    Env: Vite구성 재설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://13.125.118.210:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 });
